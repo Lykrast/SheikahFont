742f57f3d12d585ab4c3e149b137fdad670af700
Lykrast <aymericpm@gmail.com>
2025-07-13 21:13:51 +0200

    Banner patterns (except lang file)

--- src/main/java/lykrast/sheikahfont/SheikahFont.java
+++ src/main/java/lykrast/sheikahfont/SheikahFont.java
@@ -1,8 +1,14 @@
 package lykrast.sheikahfont;
 
+import net.minecraft.core.registries.Registries;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.tags.TagKey;
+import net.minecraft.world.item.BannerPatternItem;
+import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Rarity;
-import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.entity.BannerPattern;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -14,19 +20,29 @@ import net.minecraftforge.registries.RegistryObject;
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD, modid = SheikahFont.MODID)
 public class SheikahFont {
 	public static final String MODID = "sheikahfont";
-	
+
 	public SheikahFont() {
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-		BLOCKS.register(bus);
 		ITEMS.register(bus);
+		PATTERNS.register(bus);
+		bus.addListener(SheikahFont::putItemInTheTab);
 	}
 
-	private static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MODID);
+	private static final DeferredRegister<BannerPattern> PATTERNS = DeferredRegister.create(Registries.BANNER_PATTERN, MODID);
 	private static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
-	
+
 	private static RegistryObject<Item> bannerPattern;
-	
+	private static final String[] CHARACTERS = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3",
+			"4", "5", "6", "7", "8", "9", "dash", "dot", "exclamation", "question" };
+
 	static {
-		bannerPattern = ITEMS.register("banner_pattern_sheikah", () -> new Item(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON)));
+		bannerPattern = ITEMS.register("banner_pattern_sheikah", () -> new BannerPatternItem(TagKey.create(Registries.BANNER_PATTERN, new ResourceLocation(MODID, "pattern_item/sheikah")), new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON)));
+		for (String s : CHARACTERS) {
+			PATTERNS.register("sheikah_" + s, () -> new BannerPattern("sheikah_" + s));
+		}
+	}
+
+	public static void putItemInTheTab(BuildCreativeModeTabContentsEvent event) {
+		if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) event.accept(bannerPattern);
 	}
 }
